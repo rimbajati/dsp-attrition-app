5dc577606ba7c8f5a9035fe1920f4d043195000f
Rimba Jati Dwi Djatmiko <rimbajati54@gmail.com>
2025-10-28 23:27:12 +0700

    Initial commit: Flask Attrition App

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/dashboard')
+def get_dashboard():
+    return render_template('dashboard_view.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
